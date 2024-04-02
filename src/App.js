37f66d4d6d9dd51ec91adceed4df71f88e0ff0ea
Ongun <info@ongunakay.com>
2024-04-03 00:53:27 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
       <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to DuoBingo! Get ready to challenge your friends or play solo in this exciting bingo experience. 
-          Mark off the numbers as they're called out and aim to complete rows, columns, or diagonals to win big. 
-          Whether you're a seasoned bingo pro or a newcomer, DuoBingo offers endless fun for players of all skill levels. Let the game begin!
+        <p>Welcome to DuoBingo! Get ready to challenge your friends or play solo in this exciting bingo experience.
         </p>
 
         <a
