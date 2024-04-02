54038c7e311fef798092751cd6819ea1b6116483
Ongun <info@ongunakay.com>
2024-04-03 00:51:59 +0300

    modified:   src/App.js ;

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
       <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Welcome to DuoBingo! Get ready to challenge your friends or play solo in this exciting bingo experience. 
+          Mark off the numbers as they're called out and aim to complete rows, columns, or diagonals to win big. 
+          Whether you're a seasoned bingo pro or a newcomer, DuoBingo offers endless fun for players of all skill levels. Let the game begin!
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
