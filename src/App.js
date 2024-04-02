2b280865ecdde34d242280caf45ed5666ae1d3f1
Ongun <info@ongunakay.com>
2024-04-03 01:07:40 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <header className="App-header">
-        <img src={DuobingoLogo} className="App-logo" alt="logo" /> {/* */}
+        <img src={DuobingoLogo} className="App-logo" alt="logo" style={{ animation: 'none' }} /> {/* */}
         <p>Welcome to DuoBingo! Get ready to challenge your friends or play solo in this exciting bingo experience.
         </p>
 
