33abb635ed311d6b0198f2264e6dbb7cfca430b2
Ongun <info@ongunakay.com>
2024-05-26 00:58:11 +0300

    package json updated.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Import your Vue Router configuration
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const app = createApp(App);
 app.use(router); // Use Vue Router
