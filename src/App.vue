6b07081ac02e5c711022ac7c043b66132ef343e1
Ongun <info@ongunakay.com>
2024-05-25 23:09:19 +0300

    bootstrap updated.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
