01586942c696dec45e4b9df3b2ecc4b2584fde48
Ongun <info@ongunakay.com>
2024-05-25 23:47:08 +0300

    Header updated.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,22 +2,11 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">
-          <img alt="Duolingo" src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" />
-          Duolingo
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <a class="navbar-brand" href="#">Duolingo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-collapse" aria-controls="nav-collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="nav-collapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
               <a class="nav-link" href="#">Home</a>
@@ -40,10 +29,10 @@
 
 <script>
 export default {
-  name: 'AppHeader',
+  // You can add any logic or data properties here if needed
 };
 </script>
 
 <style scoped>
-/* Add your component-specific styles here */
+/* Add any custom CSS styles here */
 </style>
